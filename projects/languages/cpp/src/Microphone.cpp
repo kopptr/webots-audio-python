9d3e4fcad4524793c91126d13f7611ab84a7ad69
Tim Kopp <tkopp@srcinc.com>
2012-06-26 10:13:04 -0400

    Added Microphone.cpp. Untested

---
+++ projects/languages/cpp/src/Microphone.cpp
@@ -0,0 +1,30 @@
+/**********************************************************************************/
+/* File:          Microphone.cpp                                                  */
+/* Date:          June 12                                                         */
+/* Description:   Header file for the OO interface                                */
+/* Authors:       Tim Kopp                                                        */
+/* Copyright (c) 2012 SRC Inc. - www.srcinc.com                                   */
+/**********************************************************************************/
+
+#define WB_ALLOW_MIXING_C_AND_CPP_API
+#include <webots/Microphone.hpp>
+#include <webots/microphone.h>
+
+using namespace webots;
+
+void Microphone::enable(int ms) {
+   wb_microphone_enable(getTag(), ms);
+}
+
+void Microphone::disable() {
+   wb_microphone_disable(getTag());
+}
+
+const void *Microphone::getSampleData() {
+   return wb_microphone_get_sample_data(getTag());
+}
+
+int Microphone::getSampleSize() const {
+   return wb_microphone_get_sample_size(getTag())
+}
+
