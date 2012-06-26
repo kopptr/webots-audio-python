7803f44586c9eb46ea34ea93ed49eb6c9bbd898d
Tim Kopp <tkopp@srcinc.com>
2012-06-26 10:05:57 -0400

    Implemented Speaker.cpp. Untested

---
+++ projects/languages/cpp/src/Speaker.cpp
@@ -0,0 +1,17 @@
+/**********************************************************************************/
+/* File:          Speaker.hpp                                                  */
+/* Date:          June 12                                                         */
+/* Description:   Header file for the OO interface                                */
+/* Authors:       Tim Kopp                                                        */
+/* Copyright (c) 2012 SRC Inc. - www.srcinc.com                                   */
+/**********************************************************************************/
+
+#define WB_ALLOW_MIXING_C_AND_CPP_API
+#include <webots/Speaker.hpp>
+#include <webots/speaker.h>
+
+using namespace webots;
+
+void Speaker::emitSample(const void *data, int size) {
+   return wb_speaker_emit_sample(getTag(), data, size)
+}
