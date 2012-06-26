e64b2c5a4c53baa10993bb17c678c7a4ae422445
Tim Kopp <tkopp@srcinc.com>
2012-06-26 09:59:26 -0400

    Added Speaker.hpp. Untested

---
+++ include/controller/cpp/webots/Speaker.hpp
@@ -0,0 +1,25 @@
+/**********************************************************************************/
+/* File:          Speaker.hpp                                                  */
+/* Date:          June 12                                                         */
+/* Description:   Header file for the OO interface                                */
+/* Authors:       Tim Kopp                                                        */
+/* Copyright (c) 2012 SRC Inc. - www.srcinc.com                                   */
+/**********************************************************************************/
+
+#ifndef SPEAKER_HPP
+#define SPEAKER_HPP
+
+#include <webots/Device.hpp>
+
+namespace webots {
+   class Speaker : public Device {
+      public:
+         Speaker(const std::string &name) : Device(name) {} // Use Robot::getSpeaker() instead
+         virtual ~Speaker() {}
+         virtual void emitSample(const void *data, int size);
+   };
+}
+
+#endif //SPEAKER_HPP
+
+
