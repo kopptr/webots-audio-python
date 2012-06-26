4c80ac7d1351890fa14826876e7164d3d5c4a973
Tim Kopp <tkopp@srcinc.com>
2012-06-26 09:29:10 -0400

    Updated un/install scripts

--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@
 
 INCLUDE=include/controller/cpp/webots
 SRC=projects/languages/cpp/src
+INTERFACE=projects/languages/python/src
 
 echo 'Backing up old files...'
 mv ${WEBOTS_HOME}/${INCLUDE}/Robot.hpp ${WEBOTS_HOME}/${INCLUDE}/Robot.hpp.backup
 mv ${WEBOTS_HOME}/${SRC}/Robot.cpp ${WEBOTS_HOME}/${SRC}/Robot.cpp.backup
+mv ${WEBOTS_HOME}/${INTERFACE}/controller.i ${WEBOTS_HOME}/${INTERFACE}/controller.i.backup
 
 echo 'Copying in new files...'
 cp ./${INCLUDE}/*.hpp ${WEBOTS_HOME}/${INCLUDE}/
 cp ./${SRC}/*.cpp ${WEBOTS_HOME}/${SRC}/
+cp ./${INTERFACE}/controller.i ${WEBOTS_HOME}/${INTERFACE}/
 
 echo 'Building the C++ wrapper...'
 cd ${WEBOTS_HOME}/projects/languages/cpp/src
--- uninstall.sh
+++ uninstall.sh
@@ -4,10 +4,12 @@
 
 INCLUDE=include/controller/cpp/webots
 SRC=projects/languages/cpp/src
+INTERFACE=projects/languages/python/src
 
 echo 'Restoring the backups...'
 mv ${WEBOTS_HOME}/${INCLUDE}/Robot.hpp.backup ${WEBOTS_HOME}/${INCLUDE}/Robot.hpp
 mv ${WEBOTS_HOME}/${SRC}/Robot.cpp.backup ${WEBOTS_HOME}/${SRC}/Robot.cpp
+mv ${WEBOTS_HOME}/${INTERFACE}/controller.i.backup ${WEBOTS_HOME}/${INTERFACE}/controller.i
 
 echo 'Removing unneeded files...'
 rm ${WEBOTS_HOME}/${INCLUDE}/{Microphone,Speaker}.hpp
